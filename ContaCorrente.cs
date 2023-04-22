9918dc86f5b4dbcad71f7ef11bbd01c285e4882a
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2023-04-21 23:40:06 -0300

    Create ContaCorrente.cs

---
+++ ContaCorrente.cs
@@ -0,0 +1,9 @@
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numConta;
+
+    //Atribuindo o valor estou definindo que o saldo padrão de qualquer conta criada será de 100 e não de 0, como se a pessoa ganhasse um bônus por ter aberto a conta naquele banco.
+    public double saldo = 100;
+}
